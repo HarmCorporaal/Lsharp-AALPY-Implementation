80e56475ab598a01db8cf41af21902c4b1cbc282
Harm Corporaal <75938614+HarmCorporaal@users.noreply.github.com>
2024-09-20 11:24:49 +0200

    Delete Lsharp/test.py

--- Lsharp/test.py
+++
@@ -1,8 +0,0 @@
-from IO.FileManager import FileManager
-
-fm = FileManager()
-try:
-    mealy_machine = fm.loadMealyFromDotFile("LoesTarget.dot")
-    print(mealy_machine)
-except Exception as e:
-    print(f"Error: {e}")
